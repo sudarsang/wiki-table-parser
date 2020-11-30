07713c3fcd6b7742d45d62d62ab51922f4b4cee8
Suda_G <sutharshan.g@gmail.com>
2020-12-01 01:35:20 +1100

    LineGraphOutput more documentation and comments added

--- src/main/java/org/sudarsan/wikitableparser/chart/LineGraphOutput.java
+++ src/main/java/org/sudarsan/wikitableparser/chart/LineGraphOutput.java
@@ -18,14 +18,22 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * This class is an implementation of the GraphOutput to draw a Line Graph using the ChartData
+ */
 public class LineGraphOutput extends Application implements GraphOutput {
     private static List<ChartData> chartData;
     private static final String CHART_OUTPUT_PATH = ".\\output\\graph.png";
     private static final int CHART_WIDTH_PIXEL = 800;
     private static final int CHART_HEIGHT_PIXEL = 600;
 
+    /**
+     * The graph draw logic lives here. This uses the chartData populated already and build a chart and then save it.
+     * All the configurations for the graph and output path are configured in the class level, so in future they can be dynamically passed in.
+     * @param stage the primary stage for this application, onto which the application scene can be set.
+     */
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         final CategoryAxis xAxis = new CategoryAxis();
         final NumberAxis yAxis = new NumberAxis();
 
@@ -41,7 +49,6 @@ public class LineGraphOutput extends Application implements GraphOutput {
         lineChart.getData().add(series);
         Scene scene  = new Scene(lineChart,CHART_WIDTH_PIXEL,CHART_HEIGHT_PIXEL);
         stage.setScene(scene);
-//        stage.show();     //useful to test the output without saving
 
         try {
             WritableImage image = lineChart.snapshot(new SnapshotParameters(), new WritableImage(CHART_WIDTH_PIXEL, CHART_HEIGHT_PIXEL));
@@ -55,6 +62,11 @@ public class LineGraphOutput extends Application implements GraphOutput {
         }
     }
 
+    /**
+     * This method accepts a list of ChartData objects and plot the chart and
+     * save the chart as an image in the specified path.
+     * @param chartData list of ChartData objects which are extracted from the Wiki table.
+     */
     @Override
     public void drawChart(List<ChartData> chartData) {
         if(chartData == null || chartData.size() == 0) {
